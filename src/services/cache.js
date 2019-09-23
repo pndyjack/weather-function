db6ef48e350c5be7b2d493f5fac9ac0721fb71fb
Jaimin Pandya <pndyjack@gmail.com>
2019-09-23 16:52:41 -0400

    Updated cache location

--- src/services/cache.js
+++ src/services/cache.js
@@ -2,11 +2,11 @@
 import storage from 'node-persist';
 
 export async function saveToCache({ key, data }) {
-  await storage.init();
+  await storage.init({ dir: '~' });
   await storage.setItem(key, data);
 }
 
 export async function getFromCache({ key }) {
-  await storage.init();
+  await storage.init({ dir: '~' });
   return storage.getItem(key);
 }
