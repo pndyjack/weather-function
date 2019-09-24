4bb2d4801f89bf4077817ca8a5be2c204ccceb65
Jaimin Pandya <pndyjack@gmail.com>
2019-09-24 16:51:04 -0400

    Updated cache location

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import storage from 'node-persist';
+import os from 'os';
 
 export async function saveToCache({ key, data }) {
-  await storage.init({ dir: '~' });
+  await storage.init({ dir: os.homedir() });
   await storage.setItem(key, data);
 }
 
 export async function getFromCache({ key }) {
-  await storage.init({ dir: '~' });
+  await storage.init({ dir: os.homedir() });
   return storage.getItem(key);
 }
